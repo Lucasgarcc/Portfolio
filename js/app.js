ce2a96a58dd17e40480f4d67582fdef5924ead88
Garcia <luuccasgarciia@gmail.com>
2023-07-10 22:27:26 -0300

    Modified CSS code placed in a single folder

---
+++ js/app.js
@@ -0,0 +1,25 @@
+/*selecionar o botão */
+const btnMobile = document.getElementById('btn-mobile');
+/* função do botão */
+btnMobile
+function toggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefault();
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+    const active = nav.classList.constains('active');
+    event.currentTarget.setAttribute('aria-expanded', active);
+  if (active) {
+   event. currentTarget.setAtrribgute('aria-expanded', 'Fechar Menu');
+  } else  {
+    event. currentTarget.setAtrribgute('aria-expanded', 'Abrir Menu');
+  }
+
+}
+/* evento de clicar no botão */
+btnMobile.addEventListener( 'click', toggleMenu);
+
+
+
+/* animationc */
+
+onload.window.onload = () => ativar.addEventListener('load')
