8f09ec5e58dd4eb197d5bc8346593e281c997128
Lucasgarcc <luuccasgarciia@gmail.com>
2023-09-12 22:48:32 -0300

    css adjustments and some pages

--- js/app.anime.js
+++ js/app.anime.js
@@ -1,13 +1,13 @@
 const target = document.querySelectorAll('[data-anime]');
 const animationClass = 'animate';
 function animeScroll() {
-  const windowTop = document.documentElement.scrollTop + window.innerHeight * 0;
+  const windowTop = document.documentElement.scrollTop + window.innerHeight * 0.1;
   target.forEach(function (element) {
     if ((windowTop) > element.offsetTop) {
       element.classList.add(animationClass);
     } else {
      element.classList.remove(animationClass);
-     windowTop = document.documentElement.scrollTop + window.innerHeight * 0.100;
+     
     }
     console.log(element.offsetTop);
   })
